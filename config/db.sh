970d5feeb82305562d5a3475f3b0e0f718237453
Simon Martín <oleurud@gmail.com>
2018-08-28 20:06:10 +0200

    fix lose semicolons

--- config/db.sh
+++ config/db.sh
@@ -2,11 +2,11 @@
 
 NAME='starter'
 
-if [ -n "$1" ] then
+if [ -n "$1" ]; then
     NAME=$1
 fi
 
-psql -U postgres -c "CREATE USER $NAME"
-psql -U postgres -c "CREATE DATABASE $NAME"
-psql -U postgres -c "alter user $NAME with encrypted password '$NAME'"
-psql -U postgres -c "grant all privileges on database $NAME to $NAME"
+psql -U postgres -c "CREATE USER $NAME;"
+psql -U postgres -c "CREATE DATABASE $NAME;"
+psql -U postgres -c "alter user $NAME with encrypted password '$NAME';"
+psql -U postgres -c "grant all privileges on database $NAME to $NAME;"
